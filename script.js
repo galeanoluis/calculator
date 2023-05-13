5ce814e9d378d1a9d58f8ca96fb1dd967ca2da0d
galeanoluis <luuismiguelgaleano@gmail.com>
2023-05-13 14:13:14 -0300

    Completed Calculator visual

---
+++ script.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+    return a + b;
+}
+function substract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return Math.floor(a / b);
+}
+
